674cdc5f44c98aadc29020011652f37918e35450
Stefano Zacchiroli <zack@upsilon.cc>
2017-08-29 14:21:57 +0200

    import template from swh-py-template (init-py-repo)

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+def parse_requirements():
+    requirements = []
+    for reqf in ('requirements.txt', 'requirements-swh.txt'):
+        with open(reqf) as f:
+            for line in f.readlines():
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                requirements.append(line)
+    return requirements
+
+
+# Edit this part to match your module
+# full sample: https://forge.softwareheritage.org/diffusion/DCORE/browse/master/setup.py
+setup(
+    name='swh.<module-name>',
+    description='Software Heritage <Module\'s intent>',
+    author='Software Heritage developers',
+    author_email='swh-devel@inria.fr',
+    url='https://forge.softwareheritage.org/diffusion/<module-git-code>',
+    packages=[],  # packages's modules
+    scripts=[],   # scripts to package
+    install_requires=parse_requirements(),
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    include_package_data=True,
+)
