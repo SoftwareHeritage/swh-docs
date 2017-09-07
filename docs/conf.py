25e9826ba54abee233255c5e18dc5639fca42aba
Stefano Zacchiroli <zack@upsilon.cc>
2017-09-07 10:50:38 +0200

    conf.py: initialize Django to allow importing swh-web modules

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,9 @@
 from swh.docs.sphinx.conf import *  # NoQA
+
+# For swh-web, needed to allow import of django modules (sigh). Note: this is
+# here and not in swh.docs.sphinx.docs to avoid forcing a django dependency on
+# every module that uses the common Sphinx configuration.
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swh.web.settings")
+django.setup()
