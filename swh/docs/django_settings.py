e98950fced4bea0ccd5fb79b445fdb58a7586943
David Douard <david.douard@sdfa3.org>
2018-11-23 12:06:15 +0100

    Ugly hackish fix to try to make autodoc happy with django

--- swh/docs/django_settings.py
+++ swh/docs/django_settings.py
@@ -1,4 +1,14 @@
-#from swh.web.settings.development import *  # noqa
 from swh.deposit.settings.development import *  # noqa
+import swh.web.settings.development as web
+
+
+# merge some config variables
+ns = globals()
+for var in dir(web):
+    if var.isupper() and var in ns and isinstance(ns[var], list):
+        for elt in getattr(web, var):
+            if elt not in ns[var]:
+                ns[var].append(elt)
+
 
 SECRET_KEY = 'change me'
