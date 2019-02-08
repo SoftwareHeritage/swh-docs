549b1c5a80a1edb6f6b7331e6ccce074bd9ea578
Antoine Lambert <antoine.lambert@inria.fr>
2019-02-08 15:31:01 +0100

    Add missing favicon

--- swh/docs/sphinx/conf.py
+++ swh/docs/sphinx/conf.py
@@ -81,6 +81,8 @@ todo_include_todos = True
 #
 html_theme = 'alabaster'
 
+html_favicon = '_static/favicon.ico'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
